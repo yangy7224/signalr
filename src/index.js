9964908f41be282012aa8a42a452358a54942862
yangy <yangy@7224.com>
2019-03-14 16:24:28 +0800

    修复错误提交
    修复错误提交

--- src/index.js
+++ src/index.js
@@ -1,156 +1,120 @@
-(function (global, factory) {
-    if (typeof define === "function" && define.amd) {
-        define(['exports', '~/bower_components/config/dist/index.js', '@aspnet/signalr', './miniProgramSignalr.js'], factory);
-    } else if (typeof exports !== "undefined") {
-        factory(exports, require('~/bower_components/config/dist/index.js'), require('@aspnet/signalr'), require('./miniProgramSignalr.js'));
-    } else {
-        var mod = {
-            exports: {}
-        };
-        factory(mod.exports, global.index, global.signalr, global.miniProgramSignalr);
-        global.index = mod.exports;
+/**
+ * Created by yiyang1990 on 2019/2/25.
+ */
+let isMiniProgram = !Boolean(window);
+
+const InterfaceConfig = require(isMiniProgram? '../../../bower_components/config/dist/index.js' : '~/bower_components/config/dist/index.js').default;
+const Hub = require(isMiniProgram ? './miniProgramSignalr.js' : '@aspnet/signalr');
+
+const CommandType = {
+    SendMsgName: 'sendMessage',
+    ReceiveMsgName: 'receive',
+    ErrorMsgName: 'system'
+};
+
+class SignalR {
+    constructor(options){
+        this.hubConnection = {};
+        this.options = options;
+        this.reBuildInstance = 0;
+        this.reConnectionTime = options.reConnectionTime || 3000;
+
+        this.init();
+        this.buildConnection();
     }
-})(this, function (exports, _index, _require, _require2) {
-    'use strict';
+    //初始化消息中心
+    init(){
+        if(isMiniProgram){
+            this.hubConnection = new Hub.HubConnection();
+        }else{
+            this.hubConnection = new Hub.HubConnectionBuilder().withUrl(`${InterfaceConfig.getCurrentConfigUrl()}/chat?token=${this.options.token}`,{
+                skipNegotiation: true,
+                transport: Hub.HttpTransportType.WebSockets
+            }).configureLogging(Hub.LogLevel.Information).build();
+        }
 
-    Object.defineProperty(exports, "__esModule", {
-        value: true
-    });
-
-    var _index2 = _interopRequireDefault(_index);
-
-    function _interopRequireDefault(obj) {
-        return obj && obj.__esModule ? obj : {
-            default: obj
-        };
     }
 
-    function _toConsumableArray(arr) {
-        if (Array.isArray(arr)) {
-            for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) {
-                arr2[i] = arr[i];
-            }
 
-            return arr2;
-        } else {
-            return Array.from(arr);
+    //启动消息中心
+    start(){
+        const that = this;
+        let promise = null;
+
+        if(isMiniProgram){
+            promise = this.hubConnection.start(`${InterfaceConfig.getCurrentConfigUrl()}/chat`, {token: this.options.token});
+        }else {
+            promise = this.hubConnection.start();
+        }
+        promise && promise.then(function () {
+            console.log('连接成功');
+            clearInterval(that.reBuildInstance);
+            that.register();
+        }).catch(err => {
+            console.log('连接失败');
+            that.rebuildConnetion(err);
+        });
+
+        this.hubConnection.onclose((err) => { this.onClose(err) });
+    }
+
+    // 停止消息中心
+    stop(){
+        this.hubConnection.stop();
+    }
+
+    //注册一些基本事件
+    register(){
+        this.hubConnection.on(CommandType.SendMsgName, () => {});
+        this.hubConnection.on(CommandType.ReceiveMsgName, (res) => {this.onReceive(res)});
+        this.hubConnection.on(CommandType.ErrorMsgName, (res) => {this.onError(res)});
+    }
+
+    //发送消息
+    sendMessage(relationType, relationId, toUserId, dialogueId, msg){
+        if(isMiniProgram){
+            this.hubConnection.send(CommandType.SendMsgName, ...Array.prototype.slice.call(arguments));
+        }else {
+            this.hubConnection.invoke(CommandType.SendMsgName, ...Array.prototype.slice.call(arguments));
         }
     }
 
-    function _classCallCheck(instance, Constructor) {
-        if (!(instance instanceof Constructor)) {
-            throw new TypeError("Cannot call a class as a function");
-        }
+    // 监听接受事件
+    onReceive(res){
+
+    }
+    // 监听系统事件
+    onError(res){
+
     }
 
-    var _createClass = function () {
-        function defineProperties(target, props) {
-            for (var i = 0; i < props.length; i++) {
-                var descriptor = props[i];
-                descriptor.enumerable = descriptor.enumerable || false;
-                descriptor.configurable = true;
-                if ("value" in descriptor) descriptor.writable = true;
-                Object.defineProperty(target, descriptor.key, descriptor);
-            }
+    //监听网络断开
+    onClose(err){
+        console.log(err);
+        //断线重连
+        this.rebuildConnetion();
+    }
+
+    // 建立连接
+    buildConnection(){
+        this.start();
+        // this.register();
+    }
+
+    // 断线重连
+    rebuildConnetion(err){
+        const that = this;
+        if(that.reBuildInstance > 0){
+            return;
         }
+        err && console.log(err.toString());
 
-        return function (Constructor, protoProps, staticProps) {
-            if (protoProps) defineProperties(Constructor.prototype, protoProps);
-            if (staticProps) defineProperties(Constructor, staticProps);
-            return Constructor;
-        };
-    }();
+        that.reBuildInstance = setInterval(function () {
+            that.buildConnection();
+            console.log('正在尝试重连-----');
+        },that.reConnectionTime);
+    }
+}
 
-    var HubConnectionBuilder = _require.HubConnectionBuilder,
-        HttpTransportType = _require.HttpTransportType,
-        LogLevel = _require.LogLevel;
-    var HubConnection = _require2.HubConnection;
+export default SignalR
 
-
-    var ua = window.navigator.userAgent.toLowerCase();
-    var isMiniProgram = !Boolean(ua.indexOf('micromessenger') == -1);
-
-    var CommandType = {
-        SendMsgName: 'sendMessage',
-        ReceiveMsgName: 'receive',
-        ErrorMsgName: 'system'
-    };
-
-    var SignalR = function () {
-        function SignalR(params) {
-            _classCallCheck(this, SignalR);
-
-            this.connection = {};
-            this.options = params;
-            this.response = {};
-            this.init();
-            this.start();
-            this.register();
-        }
-        //初始化消息中心
-
-
-        _createClass(SignalR, [{
-            key: 'init',
-            value: function init() {
-                if (isMiniProgram) {
-                    this.connection = new HubConnection();
-                } else {
-                    this.connection = new HubConnectionBuilder().withUrl(_index2.default.getCurrentConfigUrl() + '/chat?token=' + this.options.token, {
-                        skipNegotiation: true,
-                        transport: HttpTransportType.WebSockets
-                    }).configureLogging(LogLevel.Information).build();
-                }
-            }
-        }, {
-            key: 'start',
-            value: function start() {
-                if (isMiniProgram) {
-                    this.connection.start(_index2.default.getCurrentConfigUrl() + '/chat', { token: this.options.token }).catch(function (err) {
-                        return console.error(err.toString());
-                    });
-                } else {
-                    this.connection.start().catch(function (err) {
-                        return console.error(err.toString());
-                    });
-                }
-            }
-        }, {
-            key: 'register',
-            value: function register() {
-                var _this = this;
-
-                this.connection.on(CommandType.SendMsgName, function () {});
-                this.connection.on(CommandType.ReceiveMsgName, function (res) {
-                    _this.onReceive(res);
-                });
-                this.connection.on(CommandType.ErrorMsgName, function (res) {
-                    _this.onError(res);
-                });
-            }
-        }, {
-            key: 'sendMessage',
-            value: function sendMessage(relationType, relationId, toUserId, msg) {
-                if (isMiniProgram) {
-                    var _connection;
-
-                    (_connection = this.connection).send.apply(_connection, [CommandType.SendMsgName].concat(_toConsumableArray(Array.prototype.slice.call(arguments))));
-                } else {
-                    var _connection2;
-
-                    (_connection2 = this.connection).invoke.apply(_connection2, [CommandType.SendMsgName].concat(_toConsumableArray(Array.prototype.slice.call(arguments))));
-                }
-            }
-        }, {
-            key: 'onReceive',
-            value: function onReceive(res) {}
-        }, {
-            key: 'onError',
-            value: function onError(res) {}
-        }]);
-
-        return SignalR;
-    }();
-
-    exports.default = SignalR;
-});
